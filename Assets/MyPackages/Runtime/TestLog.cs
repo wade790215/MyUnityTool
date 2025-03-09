4c0aa5ab35800dc939f30181c13b150fe67d3876
Bachider <w0652039@gmail.com>
2025-03-09 18:50:04 +0800

    測試用log

---
+++ Assets/MyPackages/Runtime/TestLog.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLog : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("TestLog");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
