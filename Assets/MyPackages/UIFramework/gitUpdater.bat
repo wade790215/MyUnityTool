fa51b7833ace92c162844bf9662298495aeddefc
Bachider <w0652039@gmail.com>
2025-03-26 07:46:48 +0800

    新增gitUpdater

---
+++ Assets/MyPackages/UIFramework/gitUpdater.bat
@@ -0,0 +1,36 @@
+set projectPath=%cd%
+cd /d %projectPath%
+
+echo "Update all branch to latest develop--------------------------------------"
+git checkout develop
+git fetch origin
+git reset --hard origin/develop
+git pull
+git submodule foreach --recursive git checkout develop
+git submodule foreach --recursive git fetch origin
+git submodule foreach --recursive git reset --hard origin/develop
+git submodule foreach --recursive git pull
+
+set /p curVersion=<Ver.txt
+echo "Current Version %curVersion% --------------------------------------"
+
+set /p version="Input Tag Version :"
+echo "Start tag to version %version%--------------------------------------"
+echo %version% > Ver.txt
+echo "Commit & Push Changed--------------------------------------"
+set GIT_MERGE_AUTOEDIT=no
+git commit -a -m "update submodule and change version info to %version%"
+git push
+echo "Start Release flow--------------------------------------"
+git checkout master
+git reset --hard origin/master
+git pull
+git flow release start %version%
+git flow release finish -m %version%
+git checkout master
+git push
+git checkout develop
+git push --tags
+git push
+echo "Done--------------------------------------"
+pause
